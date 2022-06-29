2f6f2ca438a8196424bc77a0c976d63052f68966
vaperLee <rize1126jesse@gmail.com>
2022-06-29 01:29:28 -0400

    take description

--- main.py
+++ main.py
@@ -15,3 +15,21 @@ with open ('secret.json') as f:
 
 KEY = secret['KEY']
 ENDPOINT = secret['ENDPOINT']
+
+computervision_client = ComputerVisionClient(ENDPOINT, CognitiveServicesCredentials(KEY))  #Authenticate the client
+
+remote_image_url = "https://raw.githubusercontent.com/Azure-Samples/cognitive-services-sample-data-files/master/ComputerVision/Images/landmark.jpg"  #Analyze image
+
+
+print("===== Describe an image - remote =====")
+# Call API with remote image
+description_results = computervision_client.describe_image(remote_image_url )
+
+# Print results with confidence score
+print("Description of the remote image: ")
+if (len(description_results.captions) == 0):
+    print("No description detected.")
+else:
+    for caption in description_results.captions:
+        print("'{}' with confidence {:.2f}%".format(caption.text, caption.confidence * 100))
+
